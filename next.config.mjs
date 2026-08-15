e3e5805a67b700d10c90385fb39eddd0bba2865a
Muhammad Umar Hakim <riskykemis665@gmail.com>
2026-08-15 11:18:29 +0700

    perbaikan: merapikan format next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Ini untuk mengabaikan error TypeScript saat build
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
